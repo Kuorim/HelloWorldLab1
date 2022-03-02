5cff286b954213f1ee6e201ed2b46c4c960ca386
Kuorim <kontrisj87@gmail.com>
2022-03-02 18:21:58 +0200

    Changes: new functionality to button

--- app/src/main/java/com/example/helloworldlab/MainActivity.java
+++ app/src/main/java/com/example/helloworldlab/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.helloworldlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text_view_Main; //butu geriau pasidaryti trumpesni pavadinima, bet kad lengviau susigaudyciau cia ir intellisense veikia tai palikau
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.text_view_Main = (TextView)findViewById(R.id.text_view_Main);
+    }
+
+    public void BtnClick(View view) {
+        this.text_view_Main = (TextView) findViewById(R.id.text_view_Main);
+        text_view_Main.setText("Hello!");
     }
 }
